43253356e37f52c0a804c4040ff5718edd0a7b73
kazamisoft <jskwak@gmail.com>
2024-04-06 14:43:22 +0900

    Fix: update rsa source

--- 01.crypt/rsa_example.c
+++ 01.crypt/rsa_example.c
@@ -1,12 +1,16 @@
+#include <stdio.h>
+#include <string.h>
 #include <openssl/rsa.h>
 #include <openssl/pem.h>
 #include <openssl/rand.h>
 
-int main() {
+int main() 
+{
     RSA *rsa;
     unsigned char plaintext[256], ciphertext[256], decrypted[256];
     int plaintext_len, ciphertext_len, decrypted_len;
 
+    strcpy(plaintext, "Hello, world");
     // RSA 키 쌍 생성
     rsa = RSA_generate_key(2048, RSA_F4, NULL, NULL);
 
@@ -16,6 +20,9 @@ int main() {
 
     // 복호화
     decrypted_len = RSA_private_decrypt(ciphertext_len, ciphertext, decrypted, rsa, RSA_PKCS1_OAEP_PADDING);
+    printf("decrypted_len=%d\n", decrypted_len);
+    printf("decrypted=[%s]\n", decrypted);
+
 
     // RSA 키 쌍 해제
     RSA_free(rsa);
